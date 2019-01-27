3488c873de1dfd7a33dd5b5d702e0503d80fad52
Malin Albinsson <mea733@telia.com>
2019-01-27 01:37:22 +0100

    Updated UserService with another get and a post method

--- src/lab2task2/UserService.java
+++ src/lab2task2/UserService.java
@@ -1,18 +1,29 @@
 package lab2task2;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.FormParam;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 
 //Själva webservicen
 @Path("/UserService")
 public class UserService {
 	UserDao userDao = new UserDao();
-	 //  private static final String SUCCESS_RESULT="<result>success</result>";
-	 //  private static final String FAILURE_RESULT="<result>failure</result>";
+	   private static final String SUCCESS_RESULT="<result>success</result>";
+	   private static final String FAILURE_RESULT="<result>failure</result>";
 
 	   
 	   @GET
@@ -20,5 +31,28 @@ public class UserService {
 	   @Produces(MediaType.APPLICATION_XML)
 	   public List<User> getUsers(){
 	      return userDao.getAllUsers();
+	   }
+	   
+	   @GET
+	   @Path("/users/{userid}")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public User getUser(@PathParam("userid") int userid){
+	      return userDao.getUser(userid);
+	   }
+	   
+	   @POST
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	   public String createUser(@FormParam("id") int id,
+	      @FormParam("name") String name,
+	      @FormParam("profession") String profession,
+	      @Context HttpServletResponse servletResponse) throws IOException{
+	      User user = new User(id, name, profession);
+	      int result = userDao.addUser(user);
+	      if(result == 1){
+	         return SUCCESS_RESULT;
+	      }
+	      return FAILURE_RESULT;
 	   }
 }
